82195c1fcd0f533e75710dfcbf909c5ea1005880
Joan Bonell Ruiz <100761163+JoanBonell@users.noreply.github.com>
2023-02-14 12:03:30 +0100

    First Commit

---
+++ app/src/main/java/udl/cat/hyperion/appmobils/thegame/MainActivity.java
@@ -0,0 +1,14 @@
+package udl.cat.hyperion.appmobils.thegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
